4b353d852c45e2c2b3c835baae52d370da1cd262
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-02-21 16:23:56 +0100

    feat: added js variables and document writeln

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+let nomeUtente = prompt("Qual è il tuo nome ?");
+console.log("nome inserito");
+
+let cognomeUtente = prompt("Qual è il tuo cognome ?");
+console.log("cognome inserito");
+
+let coloreUtente = prompt("e dimmi, qual è il tuo colore preferito ?");
+console.log("colore inserito");
+
+let combinazioneUtente = nomeUtente + cognomeUtente + coloreUtente + "21";
+
+document.writeln(combinazioneUtente);
+
+
+
